f9932b5fc6ae07fd2b6dde5b55b0baa20eb3aa82
princebongele-spec <princebongele@gmail.com>
2026-04-29 12:37:47 +0200

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
-const CACHE = 'rumchat-v3-offline';
-const URLS = [
-  './',
-  './index.html',
-  'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js',
-  'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js'
-];
+const CACHE_NAME = 'rumchat-v1';
+const urlsToCache = ['./','./index.html','./manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
